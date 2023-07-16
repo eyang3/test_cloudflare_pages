a4bf827b3d197ea9f190f5c818e223d45fbedbb2
Eric Yang <>
2023-07-16 17:39:33 -0400

    added pages

---
+++ src/views/Post.vue
@@ -0,0 +1,42 @@
+<template>
+    <div style="height: 400px">
+        <ui-editor v-model="content" style="height:10em; min-height:100%; overflow-y:auto; margin-top:3em;"></ui-editor>
+        <ui-button type="raised">Post</ui-button>
+    </div>
+</template>
+  
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    // type inference enabled
+    data() {
+        return {
+            content: '',
+            firstLoad: true
+        }
+
+
+    }
+})
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+
+>>>.mdc-editor__content {
+    height: 85%;
+    width: 80%;
+}
+
+#mdc-editor {
+    height: 100%;
+}
+</style>
---
+++ test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var clerk_js_1 = require("@clerk/clerk-js");
+console.log(clerk_js_1.default);
